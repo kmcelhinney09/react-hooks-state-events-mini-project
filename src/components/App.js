2e16adf36c3304cbc7a4ebcaaf8ea80ecff125df
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-11-22 16:09:54 -0800

    commit tree 664e0060437b426348f9a4c2458de695b8ebe0a0

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskList, setTaskList] = useState(TASKS)
+
+    
+  function removeTask(text){
+    const updatedTasks = taskList.filter(task => task.text !== text)
+    setTaskList(updatedTasks)
+  }
+  
+  function onCatagoryClick(category){
+    const updatedTasks = TASKS.filter(task => {
+      if(category==="All") return true;
+      return task.category === category
+    })
+    
+    setTaskList(updatedTasks)
+  }
+  
+  function onTaskFormSubmit(newTask){
+    setTaskList([...taskList, newTask])
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} onCatagoryClick={onCatagoryClick}/>
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={taskList} handleDeleteClick={removeTask}/>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Task from "./Task";
+import { v4 as uuid } from "uuid";
 
-function TaskList() {
+function TaskList({tasks, handleDeleteClick}) {
+
+  const tasksToDisplay = tasks.map(task => <Task key={uuid()} category={task.category} text={task.text} handleClick = {handleDeleteClick}/>)
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasksToDisplay}
     </div>
   );
 }
